f1dcaa3a1378bd2ea6f7e20a4907bae02799189e
Luna Zhang <zhliang0204@163.com>
2019-06-02 23:23:41 +0200

    update

---
+++ server/bin/seeds2.js
@@ -0,0 +1,46 @@
+const path = require('path')
+require('dotenv').config({ path: path.join(__dirname, '../.env') })
+
+
+// Seeds file that remove all users and create 2 new users
+
+// To execute this seed, run from the root of the project
+// $ node bin/seeds.js
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const AverageServiceHours = require("../models/AverageServiceHours")
+
+const bcryptSalt = 10;
+
+require('./../configs/database')
+
+let historyServiceInfo = [
+  {    
+    year: "2015",
+    aveHours: 4.5,
+    totalHours: 22.5,
+  },
+  {    
+    year: "2016",
+    aveHours: 7.3,
+    totalHours: 80.3,
+  },
+  {    
+    year: "2017",
+    aveHours: 6,
+    totalHours: 54,
+  },
+
+]
+
+User.find({role:"parent"}, {$set: {historyServiceHours:historyServiceInfo}})
+  .then(() => {
+    // Close properly the connection to Mongoose
+    mongoose.disconnect()
+  })
+  .catch(err => {
+    mongoose.disconnect()
+    throw err
+  })
